8987d1f0d97f906cb63798f864dd377ae232ac47
Gerhard Streng <gerhard.streng@mainsoft.de>
2016-01-05 15:54:04 +0100

    neue Methode für Tests

--- base.project.examples/src/base/project/examples/logic/BLClass.java
+++ base.project.examples/src/base/project/examples/logic/BLClass.java
@@ -42,6 +42,13 @@ public class BLClass {
 		}
 		return number1*number2;
 	}
+	
+	public long calculateLongNumber(long number1, long number2) {
+		if (number1 < 0) {
+			number1 *= -1;
+		}		
+		return number1*number2;
+	}
 
 	protected void doSmth3() {
 		doSmthPrivate17("hallo", "world");
